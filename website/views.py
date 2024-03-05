e4fad1a4dccdb89d87438055e8b9287e531a4c1b
ChiNguyenDev <chi.cuong.nguyen@proton.me>
2024-03-05 15:30:11 +0100

    added File Object and worked on file page. Also added File upload to Azure

--- website/views.py
+++ website/views.py
@@ -1,10 +1,18 @@
 from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
+from azure.storage.blob import BlobServiceClient, BlobClient
 from .models import File
 from . import db
+import os
 
 views = Blueprint('views', __name__)
 
+# Connect to Azure Storage
+AZURE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=quickshareproject2024;AccountKey=u8U0C0YlHt4lzm2wc1hW5GFepqKutDT9WNelPBuhujz39BPpxyX1eNr0NPPH1LbI35s97FToFuRq+AStQhuOTg==;EndpointSuffix=core.windows.net'
+CONTAINER_NAME = 'quicksharestorage'
+
+blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
+container_client = blob_service_client.get_container_client(CONTAINER_NAME)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
@@ -27,6 +35,16 @@ def upload_file():
                 db.session.add(new_file)
                 db.session.commit()
                 flash("File successfully uploaded", category="success")
+
+                # Create a unique "smart" name for the blob to avoid overwrites
+                blob_name = "uploads/{filename}".format(filename=file.filename)
+                blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=blob_name)
+
+                # Upload the file
+                blob_client.upload_blob(file)
+
+                flash('File successfully uploaded')
+
                 return redirect(url_for('views.myFiles'))
             else:
                 flash("No file selected", category="error")
