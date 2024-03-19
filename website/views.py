7a04cbbdad728af309e8d642aa9a78fc0194dd65
ChiNguyenDev <chi.cuong.nguyen@proton.me>
2024-03-19 03:10:59 +0100

    added background, implemented Azure Key Vault

--- website/views.py
+++ website/views.py
@@ -3,13 +3,21 @@ from flask_login import login_required, current_user
 from azure.storage.blob import BlobServiceClient, BlobClient
 from .models import File
 from . import db
-import os
+import uuid
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 views = Blueprint('views', __name__)
 
+# Encryption with Key vault
+key_vault_url = "https://keys-quickshare.vault.azure.net/"
+credential = DefaultAzureCredential()
+secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
+
+AZURE_CONNECTION_STRING = secret_client.get_secret("keys-quickshare").value
+
 # Connect to Azure Storage
-AZURE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=quickshareproject2024;AccountKey=u8U0C0YlHt4lzm2wc1hW5GFepqKutDT9WNelPBuhujz39BPpxyX1eNr0NPPH1LbI35s97FToFuRq+AStQhuOTg==;EndpointSuffix=core.windows.net'
-CONTAINER_NAME = 'quicksharestorage'
+CONTAINER_NAME = 'quickshare-storage'
 
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
 container_client = blob_service_client.get_container_client(CONTAINER_NAME)
@@ -30,22 +38,27 @@ def upload_file():
     if request.method == "POST":
         if current_user.is_authenticated:
             file = request.files["file"]
+            # check if a file was selected
             if file:
-                new_file = File(user_id=current_user.id, name=file.filename)
-                db.session.add(new_file)
-                db.session.commit()
-                flash("File successfully uploaded", category="success")
+                # check if file is larger than 100mb(in Bytes)
+                if request.content_length < 1000000:
+                    new_file = File(user_id=current_user.id, name=file.filename)
+                    db.session.add(new_file)
+                    db.session.commit()
+                    flash("File successfully uploaded", category="success")
 
-                # Create a unique "smart" name for the blob to avoid overwrites
-                blob_name = "uploads/{filename}".format(filename=file.filename)
-                blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=blob_name)
+                    # unique blob name with uuid to prevent overwrites
+                    blob_name = f"uploads/{current_user.id}/{uuid.uuid4()}-{file.filename}"
+                    blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=blob_name)
 
-                # Upload the file
-                blob_client.upload_blob(file)
+                    blob_client.upload_blob(file)
 
-                flash('File successfully uploaded')
+                    flash('File successfully uploaded')
 
-                return redirect(url_for('views.myFiles'))
+                    return redirect(url_for('views.myFiles'))
+                else:
+                    flash("File is too large", category="error")
+                    return redirect(url_for('views.home'))
             else:
                 flash("No file selected", category="error")
                 return redirect(url_for('views.home'))
