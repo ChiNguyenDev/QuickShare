49a1f4061ff626ca207910f6e97e006cd51ea796
ChiNguyenDev <chi.cuong.nguyen@proton.me>
2024-03-20 02:53:36 +0100

    Fixed Azure Key Vault bug

--- website/views.py
+++ website/views.py
@@ -1,26 +1,35 @@
-from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
+from dotenv import load_dotenv
+# load env variables needed for Azure Vault
+load_dotenv()
+
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from azure.storage.blob import BlobServiceClient, BlobClient
+from azure.storage.blob import BlobServiceClient
 from .models import File
 from . import db
 import uuid
-from azure.identity import DefaultAzureCredential
+import os
+from azure.identity import ClientSecretCredential
 from azure.keyvault.secrets import SecretClient
+
+secret_name = "connectionstring-quickshare"
+credentials = ClientSecretCredential(
+    client_id=os.environ['AZURE_CLIENT_ID'],
+    tenant_id=os.environ['AZURE_TENANT_ID'],
+    client_secret=os.environ['AZURE_CLIENT_SECRET']
+)
+
+secret_client = SecretClient(vault_url=os.environ['AZURE_VAULT_URL'], credential=credentials)
+AZURE_CONNECTION_STRING = secret_client.get_secret(secret_name).value
+
 
 views = Blueprint('views', __name__)
 
-# Encryption with Key vault
-key_vault_url = "https://keys-quickshare.vault.azure.net/"
-credential = DefaultAzureCredential()
-secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
-
-AZURE_CONNECTION_STRING = secret_client.get_secret("keys-quickshare").value
-
 # Connect to Azure Storage
-CONTAINER_NAME = 'quickshare-storage'
-
+CONTAINER_NAME = 'container-quickshare'
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
 container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
@@ -34,32 +43,29 @@ def myFiles():
 
 
 @views.route('/upload', methods=['POST'])
+@login_required
 def upload_file():
     if request.method == "POST":
-        if current_user.is_authenticated:
-            file = request.files["file"]
-            # check if a file was selected
-            if file:
-                # check if file is larger than 100mb(in Bytes)
-                if request.content_length < 1000000:
-                    new_file = File(user_id=current_user.id, name=file.filename)
-                    db.session.add(new_file)
-                    db.session.commit()
-                    flash("File successfully uploaded", category="success")
+        file = request.files.get("file")
+        if file:
+            if request.content_length < 100 * 1024 * 1024:  # 100MB limit
+                new_file = File(user_id=current_user.id, name=file.filename)
+                db.session.add(new_file)
+                db.session.commit()
+                flash("File successfully uploaded", category="success")
 
-                    # unique blob name with uuid to prevent overwrites
-                    blob_name = f"uploads/{current_user.id}/{uuid.uuid4()}-{file.filename}"
-                    blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=blob_name)
+                blob_name = f"uploads/{current_user.id}/{uuid.uuid4()}-{file.filename}"
+                blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=blob_name)
 
-                    blob_client.upload_blob(file)
+                blob_client.upload_blob(file)
 
-                    flash('File successfully uploaded')
-
-                    return redirect(url_for('views.myFiles'))
-                else:
-                    flash("File is too large", category="error")
-                    return redirect(url_for('views.home'))
+                flash('File successfully uploaded')
+                return redirect(url_for('views.myFiles'))
             else:
-                flash("No file selected", category="error")
-                return redirect(url_for('views.home'))
+                flash("File is too large", category="error")
+        else:
+            flash("No file selected", category="error")
+
+    return redirect(url_for('views.home'))
+
 
